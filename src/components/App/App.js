7b1e3e0930e5c5c2e1db6272999f32b548e222c8
liangjiang <liangjiang>
2018-04-16 17:12:56 +0800

    忽略

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                {/* <Nav/> */}
+                <Nav/>
                 {getRouter()}
             </div>
         )
