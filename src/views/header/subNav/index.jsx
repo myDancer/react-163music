f0169ad71fa81a8017a8d0ba439b06cda7646886
liangjiang <liangjiang>
2018-04-19 20:03:41 +0800

    subNavItem

--- src/views/header/subNav/index.jsx
+++ src/views/header/subNav/index.jsx
@@ -8,12 +8,12 @@ class Login extends React.Component {
   constructor() {
     super()
     this.subMenu = [
-      { name: '推荐', id: '1' },
-      { name: '排行榜', id: '2' },
-      { name: '歌单', id: '3' },
-      { name: '主播电台', id: '4' },
-      { name: '歌手', id: '5' },
-      { name: '新碟上架', id: '6' },
+      { name: '推荐', id: 'recommend' },
+      { name: '排行榜', id: 'toplist' },
+      { name: '歌单', id: 'playlist' },
+      { name: '主播电台', id: 'djradio' },
+      { name: '歌手', id: 'artist' },
+      { name: '新碟上架', id: 'album' },
     ]
   }
   render() {
--- src/views/header/subNav/subNavItem/index.jsx
+++ src/views/header/subNav/subNavItem/index.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom'
 import './style.styl'
 
-export default class SubMenuItem extends React.Component {
+class SubMenuItem extends React.Component {
   constructor() {
     super()
-    this.state = { isActive: false }
-    this.handleClick = this.handleClick.bind(this)
-  }
-  handleClick() {
-    this.setState({ isActive: true })
+    this.isActive = false
   }
   render() {
     const subMenu = this.props.subMenu
+    if (this.props.location.pathname.indexOf(subMenu.id) !== -1) {
+      this.isActive = true
+    } else {
+      this.isActive = false
+    }
     return (
       <li className="subnav-item">
-        <NavLink to={{ pathname: `/discover/${subMenu.id}` }} onClick={this.handleClick}><span style={{ backgroundColor: this.state.isActive ? '#9B0909' : null }} >{subMenu.name}</span></NavLink>
+        <NavLink to={{ pathname: `/discover/${subMenu.id}` }}><span style={{ backgroundColor: this.isActive ? '#9B0909' : null }} >{subMenu.name}</span></NavLink>
       </li>
     )
   }
 }
+export default withRouter(SubMenuItem)
