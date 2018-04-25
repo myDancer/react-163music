1029f6dd64f1e8c10115aad92ffbd9347104336e
liangjiang <liangjiang>
2018-04-25 14:19:15 +0800

    完成左边

--- src/views/discover/discover-module/index.jsx
+++ src/views/discover/discover-module/index.jsx
@@ -13,11 +13,37 @@ const DiscoverModule = () => {
     { url: '4', name: '民谣' },
     { url: '5', name: '电子' },
   ]
+  const songList = [
+    {
+      imgUrl: 'http://p1.music.126.net/P5BW4BgCB92OTlNfnpN6LA==/19210667160665541.jpg?param=140y140', link: './123', title: '时光里，我们都是赶路人', playNum: '24w',
+    },
+    {
+      imgUrl: 'http://p1.music.126.net/P5BW4BgCB92OTlNfnpN6LA==/19210667160665541.jpg?param=140y140', link: './123', title: '时光里，我们都是赶路人', playNum: '24w',
+    },
+    {
+      imgUrl: 'http://p1.music.126.net/P5BW4BgCB92OTlNfnpN6LA==/19210667160665541.jpg?param=140y140', link: './123', title: '时光里，我们都是赶路人', playNum: '24w',
+    },
+    {
+      imgUrl: 'http://p1.music.126.net/P5BW4BgCB92OTlNfnpN6LA==/19210667160665541.jpg?param=140y140', link: './123', title: '时光里，我们都是赶路人', playNum: '24w',
+    },
+    {
+      imgUrl: 'http://p1.music.126.net/P5BW4BgCB92OTlNfnpN6LA==/19210667160665541.jpg?param=140y140', link: './123', title: '时光里，我们都是赶路人', playNum: '24w',
+    },
+    {
+      imgUrl: 'http://p1.music.126.net/P5BW4BgCB92OTlNfnpN6LA==/19210667160665541.jpg?param=140y140', link: './123', title: '时光里，我们都是赶路人', playNum: '24w',
+    },
+    {
+      imgUrl: 'http://p1.music.126.net/P5BW4BgCB92OTlNfnpN6LA==/19210667160665541.jpg?param=140y140', link: './123', title: '时光里，我们都是赶路人', playNum: '24w',
+    },
+    {
+      imgUrl: 'http://p1.music.126.net/P5BW4BgCB92OTlNfnpN6LA==/19210667160665541.jpg?param=140y140', link: './123', title: '时光里，我们都是赶路人', playNum: '24w',
+    },
+  ]
   return (
-    <div>
+    <div className="discover-module">
       <div className="g-wrap">
         <Toolbar toolbar={toolbar} />
-        <SongList />
+        <SongList songList={songList} />
       </div>
       <Login />
       <SingerList />
--- src/views/discover/songlist/index.jsx
+++ src/views/discover/songlist/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './style.styl'
 
-export default function SongList() {
+export default function SongList(props) {
   return (
     <div>
-    .
+      <ul className="songlist">
+        {props.songList.map(item => (
+          <li className="song-item">
+            <div className="item">
+              <img alt="fengmian" src={item.imgUrl} />
+              <Link className="item-a" to={item.link} />
+              <div className="bottom">
+                <Link className="icn-play" to={item.link} />
+                <span className="icn-headset" />
+                <span>{item.playNum}</span>
+              </div>
+            </div>
+            <p className="dec"><Link className="dec-a" to={item.link}>{item.title}</Link></p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
