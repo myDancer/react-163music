f6ca6a7e0018a0751a34699b50e93ec17e76d16d
liangjiang <liangjiang>
2018-04-26 10:01:23 +0800

    style

--- src/views/discover/discover-module/index.jsx
+++ src/views/discover/discover-module/index.jsx
@@ -6,13 +6,16 @@ import SingerList from '../singerlist'
 import './style.styl'
 
 const DiscoverModule = () => {
-  const toolbar = [
-    { url: '1', name: '华语' },
-    { url: '2', name: '流行' },
-    { url: '3', name: '摇滚' },
-    { url: '4', name: '民谣' },
-    { url: '5', name: '电子' },
-  ]
+  const toolbar = {
+    title: '热门推荐',
+    urlList: [
+      { url: '1', name: '华语' },
+      { url: '2', name: '流行' },
+      { url: '3', name: '摇滚' },
+      { url: '4', name: '民谣' },
+      { url: '5', name: '电子' },
+    ],
+  }
   const songList = [
     {
       imgUrl: 'http://p1.music.126.net/P5BW4BgCB92OTlNfnpN6LA==/19210667160665541.jpg?param=140y140', link: './123', title: '时光里，我们都是赶路人', playNum: '24w',
@@ -61,6 +64,10 @@ const DiscoverModule = () => {
       <div className="l-wrap">
         <div className="l-wrap1">
           <Toolbar toolbar={toolbar} />
+          <SongList songList={songList} />
+        </div>
+        <div className="l-wrap1">
+          <Toolbar toolbar={{ title: '新碟上架' }} />
           <SongList songList={songList} />
         </div>
       </div>
--- src/views/discover/toolbar/index.jsx
+++ src/views/discover/toolbar/index.jsx
@@ -4,15 +4,21 @@ import './style.styl'
 
 const Toolbar = (props) => {
   console.log(props)
-  return (
-    <div className="toolbar">
-      <Link className="title" to="/recomend">热门推荐</Link>
+  let urlList
+  if (props.toolbar.urlList) {
+    urlList = (
       <ul className="tab">
-        {props.toolbar.map(item => (
+        {props.toolbar.urlList.map(item => (
           <li key={item.url}>
-            <Link to={item.url}>{item.name}</Link>
+            <Link className="tab-a" to={item.url}>{item.name}</Link>
           </li>))}
       </ul>
+    )
+  }
+  return (
+    <div className="toolbar">
+      <Link className="title" to="/recomend">{props.toolbar.title}</Link>
+      {urlList}
       <span className="more">
         <Link to="/discover/playlist/">更多</Link>
         <i className="icn-more" />
