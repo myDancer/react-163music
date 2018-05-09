d91127057ba302fa53c8b34317735fd3ca4de1d4
liangjiang <liangjiang>
2018-05-09 16:08:35 +0800

    优化样式二

--- src/views/header/login/index.jsx
+++ src/views/header/login/index.jsx
@@ -11,20 +11,11 @@ export default class Login extends React.Component {
       { name: '新浪微博登陆', id: 'sina' },
       { name: '网易邮箱账号登陆', id: 'wy' },
     ]
-    // this.handleMouseOver = this.handleMouseOver.bind(this)
-    // this.handleMouseOut = this.handleMouseOut.bind(this)
-    // this.state = { muneShow: false }
   }
-  // handleMouseOver() {
-  //   this.setState({ muneShow: true })
-  // }
-  // handleMouseOut() {
-  //   this.setState({ muneShow: false })
-  // }
   render() {
     const menuList = this.menuList
     return (
-      <div className="login" onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
+      <div className="login">
         <a className="login-link">登陆</a>
         <div className="menu-dialog">
           <i className="icn icn-trangle" />
