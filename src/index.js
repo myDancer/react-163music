abd3fb3115bcdc6aa176e4f57fb0c31ca745f9e4
liangjiang <liangjiang>
2018-05-09 09:45:41 +0800

    hash模式

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { composeWithDevTools } from 'redux-devtools-extension' // eslint-disable-line
 import './config/mock'
 
@@ -16,9 +16,9 @@ import './common/style/fonts.css'
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
   , document.getElementById('root'),
 )
